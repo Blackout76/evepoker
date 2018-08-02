3ce75e10ed361d153ebec3954f16ecd4e3f5fbb1
olivier <olivier.leger.dev@gmail.com>
2018-08-02 22:14:07 +0200

    add user creation on oauth

--- users/models.py
+++ users/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User # to extend the basic user
 
-class UserStats(models.Model):
-	# link to user model
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	balance = models.FloatField()
-	
+class User(models.Model):
+	character_name = models.TextField()
+	character_id = models.IntegerField()
+	user_stats =  models.ForeignKey('UserStats', on_delete=models.CASCADE)
+
 	# Override unicode method
 	def __unicode__(self):
-		return self.user.username
+		return self.user.character_name
+
+class UserStats(models.Model):
+	balance = models.FloatField()
 		
 	def changeBalance(self, change):
 		newVal = self.balance + change
--- users/views.py
+++ users/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from .models import UserStats
+from .models import User, UserStats
 from .forms import UserForm
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
-from esi.esi import esi_app, esi_security, esi_verify
+from esi.esi import esi_secure, esi_security, esi_verify
 
 # get the profile
 @login_required()
@@ -17,116 +15,9 @@ def profile(request):
 	args['UserStats'] = get_object_or_404(UserStats, user=request.user)
 	return render_to_response('profile.html', args)
 
-"""
-# register a new user 
-@csrf_exempt 
-def register(request):
-	
-	context = RequestContext(request)
-	
-	registered = False
-	
-	# we want to try and register a user
-	if request.method == 'POST':
-		
-		# grab post data to user and stats forms
-		user_form = UserForm(data=request.POST)
-		stats_form = UserStatsForm(data = request.POST)
-		
-		if user_form.is_valid() and stats_form.is_valid():
-		
-			# save user
-			user = user_form.save()
-			
-			# hash and save password
-			user.set_password(user.password)
-			user.save()
-			
-			# create associated user stats, commit after changes
-			userstats = stats_form.save(commit=False)
-			userstats.user = user
-			userstats.balance = 100000
-			
-			if 'picture' in request.FILES:
-				userstats.picture = request.FILES['picture']
-			
-			userstats.save()
-			
-			# we did it!
-			registered = True
-			
-		# invalid form(s)
-		else:
-			print (user_form.errors)
-			
-	# not a POST, so just render the form
-	else:
-		user_form = UserForm()
-		stats_form = UserStatsForm()
-		
-	# render template depending on context
-	return render_to_response(
-		'register.html',
-		{'user_form': user_form, 'stats_form': stats_form, 'registered': registered},
-		context)
-		
-# user login
-@csrf_exempt 
-def user_login(request):
-	
-	context = RequestContext(request)
-	
-	# theyre attempting to log in
-	if request.method == 'POST':
-	
-		username = request.POST['username']
-		password = request.POST['password']
-		
-		user = authenticate
-		(username=username, password=password)
-		
-		# we've got a user
-		if user is not None:
-		
-			# is the account active (not banned)
-			if user.is_active:
-			
-				login(request, user)
-				return HttpResponseRedirect('/tables/')
-			
-			else:
-				return HttpResponse("Your account is disabled")
-			
-		else:
-			return HttpResponse("Invalid login details")
-			
-	# not post, so show form
-	else:
-		return render_to_response('login.html',{}, context)
-"""
 # user login
 def user_login(request):
-	"""
-	if 'token' in request.session :
-		esi_app, security, client, scopes = get_esi()
-		try:
-			security.update_token(request.session['token'])
-		except APIException as err:
-			print (err)
-			secure = security.get_auth_uri(scopes=scopes)
-			return HttpResponse("<a href='"+secure+"'><img src='" + ESI_CONNECT_IMG_BAR + "'></a>")
-		try:
-			verify = security.verify()
-		except:
-			secure = security.get_auth_uri(scopes=scopes)
-			return HttpResponse("<a href='"+secure+"'><img src='" + ESI_CONNECT_IMG_BAR + "'></a>")
-		request.session['char'] = verify
-		return HttpResponseRedirect('/')
-	else:
-		secure = security.get_auth_uri(scopes=scopes)
-		return HttpResponse("<a href='"+secure+"'><img src='" + ESI_CONNECT_IMG_BAR + "'></a>")
-	"""
-	return HttpResponseRedirect('/')
+	return HttpResponseRedirect(esi_secure)
 
 # user oauth
 def oauth(request):
@@ -136,23 +27,16 @@ def oauth(request):
 	request.session['token'] = token
 	request.session['char'] = esi_verify()
 
-	"""
-	user = UserStats.get_object(user_esi_id=id)
-	
-	# we've not got a user
-	if user is None:
-		# create associated user stats, commit after changes
-		user = User(username="")
-		userstats = UserStats()
-		userstats.user = user
-		userstats.balance = 0
-		userstats.save()
+	user = User(character_id=request.session['char']['CharacterID'])
 
-	# is the account active (not banned)
-	if not user.is_active:
-		return HttpResponse("Your account is disabled")
-	else:
-	"""
+	if user is None:
+		# Create new user
+		user = User()
+		user.character_id = request.session['char']['CharacterID']
+		user.character_name = request.session['char']['CharacterName']
+		user.user_stats = UserStats()
+		user.user_stats.balance = 0
+		user.save()
 	return HttpResponseRedirect('/tables/')
 
 
