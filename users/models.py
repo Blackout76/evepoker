70b1e7f8718c158a542cc4997cf9a6d5e4c973d1
olivier <olivier.leger.dev@gmail.com>
2018-08-02 23:00:07 +0200

    FIX oauth with user model creation

--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User # to extend the basic user
 class User(models.Model):
 	character_name = models.TextField()
 	character_id = models.IntegerField()
-	user_stats =  models.ForeignKey('UserStats', on_delete=models.CASCADE)
+	user_stats =  models.OneToOneField('UserStats', on_delete=models.CASCADE)
 
 	# Override unicode method
 	def __unicode__(self):
-		return self.user.character_name
+		return self.character_name
 
 class UserStats(models.Model):
 	balance = models.FloatField()
--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ from esi.esi import esi_secure, esi_security, esi_verify
 @login_required()
 def profile(request):
 	args = RequestContext(request)
-	args['UserStats'] = get_object_or_404(UserStats, user=request.user)
+	#args['UserStats'] = get_object_or_404(UserStats, user=request.user)
 	return render_to_response('profile.html', args)
 
 # user login
@@ -21,26 +21,28 @@ def user_login(request):
 
 # user oauth
 def oauth(request):
-	print (request)
 	code = request.GET.get('code')
 	token = esi_security.auth(code)
 	request.session['token'] = token
 	request.session['char'] = esi_verify()
 
-	user = User(character_id=request.session['char']['CharacterID'])
+	user = User.objects.filter(character_id=request.session['char']['CharacterID']).first()
 
 	if user is None:
 		# Create new user
 		user = User()
 		user.character_id = request.session['char']['CharacterID']
 		user.character_name = request.session['char']['CharacterName']
-		user.user_stats = UserStats()
-		user.user_stats.balance = 0
+		user_stats = UserStats(balance=0)
+		user_stats.save()
+		user.user_stats = user_stats
 		user.save()
+
 	return HttpResponseRedirect('/tables/')
 
 
 # user logout    
 def user_logout(request):
-	logout(request)
-	return render_to_response('index.html',{})
+	for key in request.session.keys():
+		del request.session[key]
+	return HttpResponseRedirect('/',{})
